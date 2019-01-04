6f674953fd9668777142c1e54cce63f94ad5a292
Ivo Ivanov Atanasov <ivoatanasov85@gmail.com>
2019-01-04 16:34:10 +0200

    add word and language classes

---
+++ Learning/Learning.Infrastructure/Data/Language.cs
@@ -0,0 +1,17 @@
+﻿namespace Learning.Infrastructure.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Text;
+
+    [Table("Languages")]
+    public class Language
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string LanguageName { get; set; }
+    }
+}
---
+++ Learning/Learning.Infrastructure/Data/Word.cs
@@ -0,0 +1,20 @@
+﻿namespace Learning.Infrastructure.Data
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    [Table("Word")]
+   public class Word
+    {
+        [Key]
+        public long Id { get; set; }
+        
+        public string Text { get; set; }
+
+        public string Translation { get; set; }
+  
+        public int LanguageID { get; set; }
+
+        [ForeignKey(nameof(LanguageID))]
+        public Language ForeignLanguage { get; set; }
+    }
+}
